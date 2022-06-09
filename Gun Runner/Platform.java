28f1786dc8f3d09eb091524c9f470ebd25d860f1
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2022-06-09 14:44:19 -0400

    Delete Gun Runner directory

--- Gun Runner/Platform.java
+++
@@ -1,6 +0,0 @@
-public class Platform extends Wall {
-    Platform(int x, int y, int length, int width, String picName) {
-        super(x, y, length, width, picName);
-        super.setType("Platform");
-    }
-}
